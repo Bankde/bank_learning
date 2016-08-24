792e54e67b07d0026748747fbdd087aa506a460a
Bankde <Bankde@hotmail.com>
2016-08-24 09:34:15 +0700

    edit memcpy to correct one

--- c_lang/heap/memcpy.c
+++ c_lang/heap/memcpy.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char* argv)
+int main(int argc, char** argv)
 {
 	char* a = (char *) malloc(20);
 	char* b = (char *) malloc(25);
-	a = "Hello World!\n";
+	memcpy(a, "Hello World!\n", 13);
 	memcpy(b, a, 20);
 	printf(b);
+	free(a);
+	free(b);
 	return 0;
 }
